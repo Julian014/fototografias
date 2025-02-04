98b7164b54963cc0f96cd1252d5dc6a4864cc26c
julian <amayacarlos898@gmail.com>
2025-02-04 17:04:13 -0500

    login

--- src/db.js
+++ src/db.js
@@ -1,22 +1,20 @@
-
-
-
 const mysql = require('mysql2');
 
 
 
 // Configuración del pool de conexiones
 const pool = mysql.createPool({
-    host: '34.44.185.78',
-    user: 'julian',
-    password: '1034277764C',
-    database: 'hidraulibombas',
+    host: '147.93.113.198',
+    user: 'root',
+    password: 'PuS4ENP0tvqLuWQGkG3CQ06rpzi5Q63VX3PJimxnCz62lE7M4wRsXPf92uLnil1N',
+    database: 'fotografias',
     port: 3306,
     waitForConnections: true,
-    connectionLimit: 100,  // Aumentado para permitir más conexiones simultáneas si es necesario
-    queueLimit: 0,  // Sin límite en la cola de conexiones
-    connectTimeout: 5000  // Reducido a 5 segundos para intentar conexiones más rápidas
-}).promise();  // Esto convierte el pool en una versión que utiliza promesas
+    connectionLimit: 10,  // Puedes ajustar este valor según lo necesites
+    queueLimit: 0,
+    connectTimeout: 10000  // Intentar con más tiempo
+}).promise();
+
 
 
 
